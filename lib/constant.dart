df58fb1c1aef511fb98bc940cd4cd3d20069778d
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2022-04-06 16:53:42 +0700

    add constant

---
+++ lib/constant.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// color pallete
+const backgroundColor = Color(0xffF5F5F5);
+const blueColor1 = Color(0xff2199BE);
+const blueColor2 = Color(0xff149DC8);
+const color4 = Color.fromARGB(0, 12, 12, 12);
+
+// font weight
+const light = FontWeight.w300;
+const medium = FontWeight.w500;
+const semiBold = FontWeight.w600;
+const bold = FontWeight.w700;
+
+// text style
+TextStyle primaryText = GoogleFonts.poppins(color: color4);
+
+// default margin
+const defaultMargin1 = 20.0;
+const defaultMargin2 = 25.0;
+
+// default padding
+const defaultPadding1 = 15.0;
