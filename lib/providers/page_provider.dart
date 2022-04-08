a0e827187faac83d3f6baba8c144dfd7d108b0b6
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2022-04-08 19:06:25 +0700

    modified page provider

--- lib/providers/page_provider.dart
+++ lib/providers/page_provider.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 class PageProvier with ChangeNotifier {
-  final int _currentIndex = 0;
+  int _currentIndex = 0;
 
   int get currentIndex => _currentIndex;
 
   set currentIndex(int index) {
     print(currentIndex);
+    _currentIndex = index;
     notifyListeners();
   }
 }
