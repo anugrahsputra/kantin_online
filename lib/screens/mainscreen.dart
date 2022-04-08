b07c129a3c7590eca07bf837858e27bbd26f2f96
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2022-04-08 17:58:03 +0700

    add custom bottom navbar

--- lib/screens/mainscreen.dart
+++ lib/screens/mainscreen.dart
@@ -1,10 +1,58 @@
 import 'package:flutter/material.dart';
 
+import '../constant.dart';
+
 class Mainscreen extends StatelessWidget {
   const Mainscreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Widget customNav() {
+      return ClipRRect(
+        child: BottomAppBar(
+          clipBehavior: Clip.antiAlias,
+          child: BottomNavigationBar(
+            elevation: 0,
+            backgroundColor: backgroundColor,
+            type: BottomNavigationBarType.fixed,
+            items: [
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icons/home.png',
+                  width: 24,
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icons/search_notactive.png',
+                  width: 24,
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icons/cart_notactive.png',
+                  width: 24,
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icons/user_notactive.png',
+                  width: 24,
+                ),
+                label: '',
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: customNav(),
+    );
   }
 }
