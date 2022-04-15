311aca59e425db3139b53032b3778f21cc2983eb
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2022-04-16 00:57:53 +0700

    add terlaris card widget

---
+++ lib/widget/terlaris_card.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:kantin_online/constant.dart';
+
+import '../screens/product_detail.dart';
+
+class TerlarisCard extends StatelessWidget {
+  const TerlarisCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const ProductDetail(),
+          ),
+        );
+      },
+      child: Container(
+        width: 123,
+        height: 143,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: Colors.white,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset('assets/images/cireng.png'),
+            Padding(
+              padding: const EdgeInsets.only(top: 8, left: 5),
+              child: Text(
+                'Cireng Rujak Mantap',
+                style: primaryText.copyWith(
+                  fontSize: 12,
+                ),
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 5, right: 5, top: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Rp. 17.500',
+                    style: primaryText.copyWith(fontSize: 11),
+                  ),
+                  Text(
+                    '0x',
+                    style: primaryText.copyWith(fontSize: 11),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
