aff48f7aef74aa27b55a5db6d1e51694fdcb44da
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2022-04-19 11:22:47 +0700

    create search bar (unfinished)

--- lib/screens/home/searchscreen.dart
+++ lib/screens/home/searchscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../constant.dart';
 
@@ -7,12 +8,105 @@ class Searchscreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: backgroundColor,
-      body: Center(
-        child: Text(
-          'searchscreen',
+    Widget titleBar() {
+      return Container(
+        margin: const EdgeInsets.symmetric(
+          horizontal: defaultMargin1,
+          vertical: defaultMargin1,
         ),
+        child: Text(
+          'Pencarian',
+          style: primaryText.copyWith(
+            fontSize: 18,
+            fontWeight: medium,
+          ),
+        ),
+      );
+    }
+
+    Widget searchBox() {
+      return Container(
+        margin: const EdgeInsets.only(
+          left: defaultMargin1,
+          right: defaultMargin1,
+        ),
+        width: MediaQuery.of(context).size.width,
+        height: 44,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(
+            color: const Color(0xffE1E1E1),
+            width: 0.5,
+            style: BorderStyle.solid,
+          ),
+          borderRadius: BorderRadius.circular(33),
+        ),
+        child: TextField(
+          style: primaryText.copyWith(
+            fontSize: 11,
+            fontWeight: light,
+          ),
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            prefixIcon: Align(
+              widthFactor: 1.0,
+              heightFactor: 1.0,
+              child: SvgPicture.asset(
+                'assets/icons/search.svg',
+                width: 14,
+              ),
+            ),
+            hintText: 'Cari dikantin unpas',
+            hintStyle: primaryText.copyWith(
+              fontSize: 11,
+              fontWeight: light,
+            ),
+          ),
+        ),
+      );
+    }
+
+    Widget mostSearched() {
+      return Container(
+        margin: const EdgeInsets.symmetric(
+          horizontal: defaultMargin1,
+          vertical: defaultMargin1,
+        ),
+        child: Text(
+          'Paling banyak dicari',
+          style: primaryText.copyWith(
+            fontSize: 12,
+            fontWeight: semiBold,
+          ),
+        ),
+      );
+    }
+
+    Widget searchResult() {
+      return Container(
+        margin: const EdgeInsets.symmetric(
+          horizontal: defaultMargin1,
+          vertical: defaultMargin1,
+        ),
+        child: Text(
+          'Hasil pencarian',
+          style: primaryText.copyWith(
+            fontSize: 12,
+            fontWeight: semiBold,
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: ListView(
+        children: [
+          titleBar(),
+          searchBox(),
+          // mostSearched(),
+          searchResult(),
+        ],
       ),
     );
   }
