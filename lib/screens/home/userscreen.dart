853cfd68f421abb305985410c71b62dbbd29ba87
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2022-04-21 01:44:40 +0700

    change userscreen

--- lib/screens/home/userscreen.dart
+++ lib/screens/home/userscreen.dart
@@ -7,11 +7,141 @@ class Userscreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    Widget header() {
+      return Container(
+        margin: const EdgeInsets.only(
+          left: defaultMargin2,
+          bottom: defaultMargin1,
+          top: defaultMargin2,
+        ),
+        child: Row(
+          children: [
+            Image.asset(
+              'assets/images/default_profilepic.png',
+              width: 39,
+            ),
+            const SizedBox(
+              width: 15,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Anugrah Surya Putra',
+                  style: primaryText.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                ),
+                Text(
+                  'putra.183040095@mail.unpas.ac.id',
+                  style: primaryText.copyWith(
+                    fontSize: 10,
+                    color: const Color(0xff808080),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget akun() {
+      return Container(
+        margin:
+            const EdgeInsets.only(left: defaultMargin2, right: defaultMargin2),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Akun',
+              style: primaryText.copyWith(
+                fontSize: 14,
+                fontWeight: semiBold,
+              ),
+            ),
+            const SizedBox(
+              height: 7,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 136,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(
+                  color: const Color(0xffE2E2E2),
+                ),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      margin: const EdgeInsets.only(
+                          left: defaultMargin1, top: 13, bottom: 13),
+                      child: Text(
+                        'Edit Profil',
+                        style: primaryText.copyWith(
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Divider(
+                    color: Color(0xffE2E2E2),
+                    height: 1,
+                    thickness: 1,
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: defaultMargin1, vertical: 13),
+                      child: Text(
+                        'Wishlist',
+                        style: primaryText.copyWith(
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Divider(
+                    color: Color(0xffE2E2E2),
+                    height: 1,
+                    thickness: 1,
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: defaultMargin1, vertical: 13),
+                      child: Text(
+                        'Bantuan',
+                        style: primaryText.copyWith(
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
       backgroundColor: backgroundColor,
-      body: Center(
-        child: Text(
-          'userscreen',
+      body: SafeArea(
+        child: Column(
+          children: [
+            header(),
+            akun(),
+          ],
         ),
       ),
     );
