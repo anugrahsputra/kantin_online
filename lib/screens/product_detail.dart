3fb4fae75bfc166fb94b4db9b94620993a704572
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2022-04-30 22:54:11 +0700

    add content

--- lib/screens/product_detail.dart
+++ lib/screens/product_detail.dart
@@ -7,9 +7,190 @@ class ProductDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: backgroundColor,
-      body: Center(child: Text('product detail screen')),
+    Widget banner() {
+      return Container(
+        width: MediaQuery.of(context).size.width,
+        height: 272,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/cireng_poster.png'),
+            fit: BoxFit.fill,
+          ),
+        ),
+      );
+    }
+
+    Widget productName() {
+      return Padding(
+        padding: const EdgeInsets.only(
+          left: defaultPadding1,
+          top: defaultPadding1,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Cireng Rujak Mantap',
+              style: primaryText.copyWith(
+                fontSize: 18,
+                fontWeight: semiBold,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 7),
+              child: Text(
+                '0 Penilaian | 0 Terjual',
+                style: primaryText.copyWith(
+                  fontSize: 11,
+                  color: const Color(0xff8D8D8D),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget priceTag() {
+      return Container(
+        margin: const EdgeInsets.symmetric(
+          horizontal: defaultMargin2,
+          vertical: defaultMargin2,
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 14),
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: const Color(0xffECECEC),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Text(
+          'Rp.17.500',
+          style: primaryText.copyWith(
+            fontSize: 14,
+            fontWeight: medium,
+          ),
+        ),
+      );
+    }
+
+    Widget productDetails() {
+      return Container(
+        margin: const EdgeInsets.only(left: defaultMargin2),
+        child: Row(
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width / 3,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Kategori',
+                    style: primaryText.copyWith(
+                      fontSize: 12,
+                      color: const Color(0xff8D8D8D),
+                    ),
+                  ),
+                  Text(
+                    'Total Stok',
+                    style: primaryText.copyWith(
+                      fontSize: 12,
+                      color: const Color(0xff8D8D8D),
+                    ),
+                  ),
+                  Text(
+                    'Stok Tersisa',
+                    style: primaryText.copyWith(
+                      fontSize: 12,
+                      color: const Color(0xff8D8D8D),
+                    ),
+                  ),
+                  Text(
+                    'Dikirm Dari',
+                    style: primaryText.copyWith(
+                      fontSize: 12,
+                      color: const Color(0xff8D8D8D),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Makanan Ringan',
+                  style: primaryText.copyWith(
+                    fontSize: 12,
+                  ),
+                ),
+                Text(
+                  '100',
+                  style: primaryText.copyWith(
+                    fontSize: 12,
+                  ),
+                ),
+                Text(
+                  '100',
+                  style: primaryText.copyWith(
+                    fontSize: 12,
+                  ),
+                ),
+                Text(
+                  'Cimahi, Jawa Barat, Cimahi Selatan',
+                  style: primaryText.copyWith(
+                    fontSize: 12,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget productDescription() {
+      return Padding(
+        padding: const EdgeInsets.only(
+          left: defaultMargin2,
+          top: defaultMargin2,
+          right: defaultMargin2,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Deskripsi Produk',
+              style: primaryText.copyWith(
+                fontSize: 15,
+                fontWeight: semiBold,
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 12),
+              child: Text(
+                'Dibuat dari tepung Tapioka pilihan',
+                style: primaryText.copyWith(
+                  color: const Color(0xff8D8D8D),
+                ),
+                textAlign: TextAlign.justify,
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      body: ListView(
+        children: [
+          banner(),
+          productName(),
+          priceTag(),
+          productDetails(),
+          productDescription(),
+        ],
+      ),
     );
   }
 }
