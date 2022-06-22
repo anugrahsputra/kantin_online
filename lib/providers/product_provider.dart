d1079f01c951c8d10eb9e4a9434dcdb526b62813
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2022-06-22 23:36:11 +0700

    add product data

--- lib/providers/product_provider.dart
+++ lib/providers/product_provider.dart
@@ -1,81 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:kantin_online/data/product_data.dart';
 
-import '../models/category_model.dart';
 import '../models/product_model.dart';
 
 class ProductProvider with ChangeNotifier {
-  List<ProductModel> _products = [
-    ProductModel(
-      id: 1,
-      name: 'Cireng Rujak',
-      category: CategoryModel(id: 1, name: 'Makanan Ringan'),
-      address: 'Cimahi, Jawa Barat',
-      totalStock: 100,
-      stockLeft: 100,
-      price: 17500,
-      img: 'assets/images/cireng.png',
-      description: 'Dibuat dari tepung tapioka pilihan',
-    ),
-    ProductModel(
-      id: 2,
-      name: 'Hazelnut Coffee',
-      category: CategoryModel(id: 2, name: 'Aneka Kopi'),
-      address: 'Cimahi, Jawa Barat',
-      totalStock: 100,
-      stockLeft: 100,
-      price: 20000,
-      img: 'assets/images/hazelnut.png',
-      description: 'Kopi Espresso, Susu UHT dan Syrup Huzelnut',
-    ),
-    ProductModel(
-      id: 3,
-      name: 'Java Brown Sugar Coffee',
-      category: CategoryModel(id: 3, name: 'Aneka Kopi'),
-      address: 'Cimahi, Jawa Barat',
-      totalStock: 100,
-      stockLeft: 100,
-      price: 25000,
-      img: 'assets/images/javabrown.png',
-      description: 'Kopi Espresso, Susu UHT dan Syrup Huzelnut',
-    ),
-    ProductModel(
-      id: 4,
-      name: 'Cireng Mantap',
-      category: CategoryModel(id: 4, name: 'Makanan Ringan'),
-      address: 'Cimahi, Jawa Barat',
-      totalStock: 100,
-      stockLeft: 100,
-      price: 15000,
-      img: 'assets/images/cirengmantap.png',
-      description: 'Dibuat dari tepung tapioka pilihan',
-    ),
-    ProductModel(
-      id: 5,
-      name: 'Cireng Rujak Mantap',
-      category: CategoryModel(id: 5, name: 'Makanan Ringan'),
-      address: 'Cimahi, Jawa Barat',
-      totalStock: 100,
-      stockLeft: 100,
-      price: 23000,
-      img: 'assets/images/cireng.png',
-      description: 'Dibuat dari tepung tapioka pilihan',
-    ),
-    ProductModel(
-      id: 6,
-      name: 'Cireng Mantap 2',
-      category: CategoryModel(id: 6, name: 'Makanan Ringan'),
-      address: 'Cimahi, Jawa Barat',
-      totalStock: 100,
-      stockLeft: 100,
-      price: 15000,
-      img: 'assets/images/cirengmantap2.png',
-      description: 'Dibuat dari tepung tapioka pilihan',
-    ),
-  ];
+  List<ProductModel> _products = productData;
   List<ProductModel> get products => _products;
 
   set product(List<ProductModel> products) {
     _products = products;
+    notifyListeners();
+  }
+
+  search(String query) {
+    _products = products;
+    products.where((product) {
+      final name = product.name!.toLowerCase();
+      final category = product.category.name.toLowerCase();
+      final searchLower = query.toLowerCase();
+
+      return name.contains(searchLower) || category.contains(searchLower);
+    }).toList();
+
     notifyListeners();
   }
 }
